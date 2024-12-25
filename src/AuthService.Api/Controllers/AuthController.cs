3f80d6758184a43683a8ed2e1d9cf59a77dbe3a7
Fantazirro <fantazirro@gmail.com>
2024-12-25 15:14:10 +0500

    Add endpoints and domain project

---
+++ src/AuthService.Api/Controllers/AuthController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.Api.Controllers
+{
+    [Route("auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("sign-up")]
+        public async Task<IActionResult> SignUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("sign-in")]
+        public async Task<IActionResult> SignIn()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("sign-out")]
+        public async Task<IActionResult> SignOut()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("auth")]
+        public async Task<IActionResult> Auth()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("refresh")]
+        public async Task<IActionResult> RefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("send-code")]
+        public async Task<IActionResult> SendCode()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("confirm-email")]
+        public async Task<IActionResult> ConfirmEmail()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("reset-password")]
+        public async Task<IActionResult> ResetPassword()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/AuthService.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using AuthService.Domain.Common;
+
+namespace AuthService.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Email { get; set; } = null!;
+        public string? UserName { get; set; }
+        public string PasswordHash { get; set; } = null!;
+        public bool IsEmailConfirmed { get; set; }
+    }
+}
